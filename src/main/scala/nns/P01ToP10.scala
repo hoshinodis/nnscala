dee8e931031eb39209dd5caac22aa1cbc489c61d
hoshinodis <okonomisentai@gmail.com>
2015-11-04 19:21:22 +0900

    問1-6までやった

---
+++ src/main/scala/nns/P01ToP10.scala
@@ -0,0 +1,27 @@
+package nns
+
+//TODO:パターンマッチでも解く
+object P01ToP10 {
+  def last[A](ls: List[A]) = ls.last
+
+  def penultimate[A](ls: List[A]) = ls.init.last
+
+  def nth[A](n: Int, ls: List[A]) = ls(n)
+
+  def length[A](ls: List[A]) = ls.length
+
+  def reverse[A](ls: List[A]) = ls.reverse
+
+  def isPalindrome[A](ls: List[A]) = ls == ls.reverse
+
+  def flatten[A](ls: List[A]): List[A] = ls
+
+  def compress[A](ls: List[A]): List[A] = ls match {
+    case Nil => Nil
+    case h :: tail => h :: compress(tail.dropWhile(_ == h))
+  }
+
+  def pack[A](ls: List[A]): List[List[A]] = List(ls)
+
+  def encode[A](ls: List[A]): List[List[A]] = List(ls)
+}
